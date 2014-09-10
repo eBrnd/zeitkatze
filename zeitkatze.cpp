2061a2144dd3bdc841034ba13060a00fe2c9137d
eBrnd <github@ebrnd.de>
2014-09-10 21:54:45 +0200

    print and elapsed functions

--- zeitkatze.cpp
+++ zeitkatze.cpp
@@ -5,7 +5,10 @@
 class Zeitkatze {
   public:
     Zeitkatze() : start(clock()) { }
-    ~Zeitkatze() { std::cout << (static_cast<double>(clock() - start) / static_cast<double>(CLOCKS_PER_SEC)) << std::endl; }
+    ~Zeitkatze() { print(); }
+
+    void print() { std::cout << elapsed() << std::endl; }
+    double elapsed() { return static_cast<double>(clock() - start) / static_cast<double>(CLOCKS_PER_SEC); }
   private:
     clock_t start;
 };
