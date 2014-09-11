9055a745de75d0e2534501be35934dd72a6ac340
eBrnd <github@ebrnd.de>
2014-09-11 20:35:56 +0200

    split / stop time overwrite running time

--- zeitkatze.cpp
+++ zeitkatze.cpp
@@ -9,16 +9,21 @@ using std::chrono::duration_cast;
 
 class Zeitkatze {
   public:
-    Zeitkatze() : start(steady_clock::now()) { }
-    ~Zeitkatze() { print_split_time("Stop: "); }
+    Zeitkatze() : split_printed(false), start(steady_clock::now()) { }
+    ~Zeitkatze() { print_split_time("Stop: "); std::cout << std::endl; }
 
     void print_split_time(const std::string& msg) {
-      std::cout << "\r" << std::flush << msg << elapsed() << std::endl;
+      std::cout << "\r" << std::flush << msg << elapsed() << std::flush;
+      split_printed = true;
     }
 
     void print_current_time() {
+      if (split_printed) {
+        std::cout << std::endl;
+        split_printed = false;
+      }
       std::cout << "\r" << std::flush << elapsed();
-     }
+    }
 
     double elapsed() {
       duration<double> time_span = duration_cast<duration<double>>(steady_clock::now() - start);
@@ -26,17 +31,17 @@ class Zeitkatze {
     }
 
   private:
+    bool split_printed;
     steady_clock::time_point start;
 };
 
 // oh so globally
+const double EXIT_TIMEOUT = 0.8;
 Zeitkatze z;
 bool running = true;
+double last_interrupt = -EXIT_TIMEOUT;
 
 void interrupt(int sig) {
-  const double EXIT_TIMEOUT = 0.8;
-  static double last_interrupt = -EXIT_TIMEOUT;
-
   if (z.elapsed() - last_interrupt < EXIT_TIMEOUT)
     running = false;
   else
@@ -50,7 +55,8 @@ int main(int argc, char** argv) {
 
   while(running) {
     poll(0, 0, 50);
-    z.print_current_time();
+    if (z.elapsed() - last_interrupt > EXIT_TIMEOUT)
+      z.print_current_time();
   }
 
   return 0;
