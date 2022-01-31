b6df434adc65a8753e2079e1da6dabbd65f877f2
Leo Mav <13514096+leonmavr@users.noreply.github.com>
2022-01-31 03:07:20 +0100

    Fix some indentations

--- include/zeitkatze_runner.hpp
+++ include/zeitkatze_runner.hpp
@@ -16,15 +16,15 @@ class ZeitkatzeRunner {
 		ZeitkatzeRunner (): prec_(2) { init(true); }
 		ZeitkatzeRunner (bool enable_color): prec_(2) { init(enable_color); }
 		ZeitkatzeRunner (bool enable_color, unsigned precision): prec_(precision)
-            { init(enable_color); }
+			{ init(enable_color); }
 		void run();
 
 	private:
 		// methods
 		void init(bool enable_color);
 		// members
-        // how many decimals when formating seconds in Zeitkatze instance
-        unsigned prec_;
+		// how many decimals when formating seconds in Zeitkatze instance
+		unsigned prec_;
 		std::unique_ptr<Zeitkatze> zeitkatze = std::make_unique<Zeitkatze>(prec_);
 		const double kExitTimeout_ = 0.8;
 		bool running_ = true;
