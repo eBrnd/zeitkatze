7e69766fcfc3c949350128d6e2fdef7c48acf555
leonmavr <0xleo.git@gmail.com>
2022-02-01 07:33:39 +0100

    Add main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "zeitkatze.hpp"
-#include "zeitkatze_runner.hpp"
+#include  "zeitkatze.hpp"
+
 #include <iostream>
 #include <string>
 #include <atomic>
@@ -32,41 +32,10 @@ void interrupt(int) {
 }
 
 
-int main(int argc, char** argv) {
-    std::string instructions = "Zeitkatze\n\n" 
-        "\ttime cat -- literally\n\n"
-        "Arguments:\n"
-        "[-n | --no-color] (default true) If passed, disables colours\n"
-        "[-p | --precision] (default 2) How many decimals to use when measuring time\n"
-        "Wrong/Invalid argument prints usage message and exits\n\n"
-        "Instructions:\n"
-        "\tRun the executable to start measuring time.\n"
-        "\t-- Ctr + c = Split time/lap time\n "
-        "\t-- Ctr + d = Stop and exit\n"
-        "\t-- r = Reset current lap\n\n";
 
-    // TODO: write an arg parser class
-    bool argColorEnabled = true;
-    int argPrecision = 2;
-    char** arg = argv;
-    auto argEqual = [] (char** arg, const std::string& str) -> bool
-        { return !std::string(*arg).compare(str); };
-    while(--argc > 0)	{
-        if (argEqual(arg, "-n") || argEqual(arg, "--no-color"))
-            argColorEnabled = false;
-        if (argEqual(arg, "-p") || argEqual(arg, "--precision")) {
-            if (argc >= 1)
-                argPrecision = std::stoi(std::string(*++arg));
-        }
-        if (argEqual(arg, "-h") || argEqual(arg, "--help")) {
-            std::cout << instructions;
-            return 0;
-        }
-        arg++;
-    }
 
-    color_enabled = argColorEnabled;
-    auto z = std::make_unique<ZeitkatzeRunner>(color_enabled, argPrecision);
-    z->run();
-    return 0;
+int main()
+{
+    auto z = Zeitkatze();        
+    z.run();
 }
